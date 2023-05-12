3f27f7913bab28d5f96cea8bc95d8261ca5d63b8
Ty Everett <ty@tyweb.us>
2023-05-12 13:10:25 -0700

    Remove topics and labels

--- src/createAction.js
+++ src/createAction.js
@@ -7,8 +7,6 @@ const getRandomID = require('./utils/getRandomID')
  * @param {Object} [obj.inputs] Input scripts to spend as part of this Action. This is an object whose keys are TXIDs and whose values are Everett-style transaction envelopes that contain an additional field called `outputsToRedeen`. This additional field is an array of objects, each containing `index` and `unlockingScript` properties. The `index` property is the output number in the transaction you are spending, and `unlockingScript` is the hex-encoded Bitcoin script that unlocks and spends the output. Any signatures should be created with `SIGHASH_NONE | ANYONECANPAY` so that additional modifications to the resulting transaction can be made afterward without invalidating them. You may substitute `SIGHASH_NONE` for `SIGHASH_SINGLE` if required for a token contract, or drop `ANYONECANPAY` if you are self-funding the Action.
  * @param {Array<Object>} [obj.outputs] The array of transaction outputs (amounts and scripts) that you want in the transaction. Each object contains "satoshis" and "script", which can be any custom locking script of your choosing.
  * @param {string} obj.description A present-tense description of the user Action being facilitated or represented by this BitCoin transaction.
- * @param {Array<String>} [obj.bridges=[]] Topics to notify about this Action.
- * @param {Array<String>} [obj.labels=[]] Labels to apply to this Action.
  * @param {Boolean} [obj.dangerouslyDisableMapi=false] Disables returning mAPI responses with created transaction, dramatically improving performance while removing the ability of recipients to check for double-spends by checking mAPI signatures.
  * @returns {Promise<Object>} An Action object containing "txid", "rawTx" "mapiResponses" and "inputs".
  */
@@ -16,8 +14,6 @@ module.exports = async ({
   inputs,
   outputs,
   description,
-  topics,
-  labels,
   dangerouslyDisableMapi
 }) => {
   const connection = await connectToSubstrate()
@@ -33,8 +29,6 @@ module.exports = async ({
           inputs,
           outputs,
           description,
-          topics,
-          labels,
           dangerouslyDisableMapi
         })
       }
@@ -62,8 +56,6 @@ module.exports = async ({
           inputs,
           outputs,
           description,
-          topics,
-          labels,
           dangerouslyDisableMapi
         }
       }, '*')
@@ -73,8 +65,6 @@ module.exports = async ({
       inputs,
       outputs,
       description,
-      topics,
-      labels,
       dangerouslyDisableMapi
     })
   } else {
